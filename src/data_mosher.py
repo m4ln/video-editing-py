96167eb9c08369c9f6f11619a45bd643cc047870
m4ln <runzmarlen@gmail.com>
2025-06-13 23:26:37 +0800

    mosher

--- src/data_mosher.py
+++ src/data_mosher.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import subprocess
 
+
 class DataMosher:
     def __init__(self, video, start_frames, end_frames, fps, save_path, delta):
         self.video = video
@@ -13,10 +14,13 @@ class DataMosher:
         self.save_path = save_path
         self.delta = delta
 
-        self.video_path = os.path.join(os.path.dirname(__file__), '..', 'data', self.video)
+        self.video_path = os.path.join(
+            os.path.dirname(__file__), '..', 'data', self.video)
         if not os.path.exists(self.video_path):
-            raise FileNotFoundError(f"Video file {self.video_path} does not exist in the data directory.")
-        self.results_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
+            raise FileNotFoundError(
+                f"Video file {self.video_path} does not exist in the data directory.")
+        self.results_dir = os.path.join(
+            os.path.dirname(__file__), '..', 'results')
         if not os.path.exists(self.results_dir):
             os.makedirs(self.results_dir)
 
@@ -51,7 +55,8 @@ class DataMosher:
         in_file_bytes = self.in_file.read()
         frame_start = bytes.fromhex('30306463')
         frames = in_file_bytes.split(frame_start)
-        self.out_file = open(os.path.join(self.results_dir, f"{self.save_path}_moshed.avi"), 'wb')
+        self.out_file = open(os.path.join(
+            self.results_dir, f"{self.save_path}_moshed.avi"), 'wb')
         self.out_file.write(frames[0])
         frames = frames[1:]
 
@@ -99,8 +104,10 @@ class DataMosher:
 
         self.out_file.close()
         # Export the final video
-        output_avi = os.path.join(self.results_dir, f"{self.save_path}_moshed.avi")
-        final_output = os.path.join(self.results_dir, f"{self.save_path}_moshed.mp4")
+        output_avi = os.path.join(
+            self.results_dir, f"{self.save_path}_moshed.avi")
+        final_output = os.path.join(
+            self.results_dir, f"{self.save_path}_moshed.mp4")
         subprocess.call(
             f'ffmpeg -loglevel error -y -i {output_avi} -crf 18 -pix_fmt yuv420p -vcodec libx264 -acodec aac -b 10000k -r {self.fps} {final_output}',
             shell=True
@@ -108,7 +115,7 @@ class DataMosher:
         os.remove(output_avi)
         self.cleanup()
         print(f"Final video saved to {final_output}")
-        
+
     def process_segment(self, start_frame, end_frame, output_video):
         self.start_frame = start_frame
         self.end_frame = end_frame
@@ -124,7 +131,8 @@ class DataMosher:
         iframe = bytes.fromhex('0001B0')
         pframe = bytes.fromhex('0001B6')
 
-        n_video_frames = len([frame for frame in frames if frame[5:8] == iframe or frame[5:8] == pframe])
+        n_video_frames = len(
+            [frame for frame in frames if frame[5:8] == iframe or frame[5:8] == pframe])
         end_frame = self.end_frame if self.end_frame >= 0 else n_video_frames
 
         def write_frame(frame):
@@ -172,7 +180,8 @@ class DataMosher:
     def process_and_concat(self):
         temp_videos = []
         for idx, (start, end) in enumerate(zip(self.start_frames, self.end_frames)):
-            temp_output = os.path.join(self.results_dir, f"temp_mosh_{start}_{end}.mp4")
+            temp_output = os.path.join(
+                self.results_dir, f"temp_mosh_{start}_{end}.mp4")
             temp_videos.append(temp_output)
             self.process_segment(start, end, temp_output)
 
@@ -182,7 +191,8 @@ class DataMosher:
             for video in temp_videos:
                 f.write(f"file '{video}'\n")
 
-        final_output = os.path.join(self.results_dir, f"{self.save_path}_moshed.mp4")
+        final_output = os.path.join(
+            self.results_dir, f"{self.save_path}_moshed.mp4")
         subprocess.call(
             f"ffmpeg -loglevel error -y -f concat -safe 0 -i {concat_list_path} -c copy {final_output}",
             shell=True
@@ -197,18 +207,26 @@ class DataMosher:
 
         print(f"Final video saved to {final_output}")
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--video', type=str, default='hand.mov', help='File to be moshed')
-    parser.add_argument('--start_frames', nargs='+', type=int, required=True, help='List of start frames')
-    parser.add_argument('--end_frames', nargs='+', type=int, required=True, help='List of end frames')
-    parser.add_argument('--fps', '-f', default=30, type=int, help='fps to convert initial video to')
-    parser.add_argument('--save_path', type=str, default='out', help="Base path to save processed video.")
-    parser.add_argument('--delta', '-d', default=0, type=int, help='number of delta frames to repeat')
+    parser.add_argument('--video', type=str,
+                        default='mov03.mov', help='File to be moshed')
+    parser.add_argument('--start_frames', nargs='+', type=int,
+                        required=True, help='List of start frames')
+    parser.add_argument('--end_frames', nargs='+', type=int,
+                        required=True, help='List of end frames')
+    parser.add_argument('--fps', '-f', default=30, type=int,
+                        help='fps to convert initial video to')
+    parser.add_argument('--save_path', type=str, default='out',
+                        help="Base path to save processed video.")
+    parser.add_argument('--delta', '-d', default=0, type=int,
+                        help='number of delta frames to repeat')
     args = parser.parse_args()
 
     if len(args.start_frames) != len(args.end_frames):
-        raise ValueError("start_frames and end_frames must have the same length.")
+        raise ValueError(
+            "start_frames and end_frames must have the same length.")
 
     mosher = DataMosher(
         video=args.video,
@@ -218,7 +236,12 @@ def main():
         save_path=args.save_path,
         delta=args.delta
     )
-    mosher.process_all_ranges()
+    if len(args.start_frames) == 1 and len(args.end_frames) == 1:
+        mosher.process_segment(args.start_frames[0], args.end_frames[0], os.path.join(
+            mosher.results_dir, f"{args.save_path}_moshed.mp4"))
+    else:
+        mosher.process_all_ranges()
+
 
 if __name__ == "__main__":
     main()
