fd073993f93fab3b019523afe7f4e59f1c3747a9
m4ln <runzmarlen@gmail.com>
2025-06-16 19:22:01 +0800

    .

--- src/styles/horizontal_motion_example.py
+++ src/styles/horizontal_motion_example.py
@@ -6,6 +6,7 @@ def mosh_frames(frames):
         for row in frame:
             for col in row:
                 # col contains the horizontal and vertical components of the vector
-                col[1] = 0
+                col[0] = 0  # Set horizontal component to 0 for vertical motion only
+                # col[1] = 0  # Set vertical component to 0 for horizontal motion only
 
     return frames
